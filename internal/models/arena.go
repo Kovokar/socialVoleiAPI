2607d0abd022bae9ffd6c0ef2f1bda8bf13aff87
kovokar <pkovokar@gmail.com>
2025-10-15 02:20:55 -0300

    feat: arena model

--- internal/models/arena.go
+++ internal/models/arena.go
@@ -1,18 +1,25 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Arena struct {
-	ID                  uint64    `gorm:"primaryKey;autoIncrement" json:"id"`
-	EstablishmentID     uint64    `gorm:"not null;index" json:"establishment_id" binding:"required"`
-	Name                string    `gorm:"type:varchar(255);not null" json:"name" binding:"required,min=3,max=100"`
-	CapacidadeJogadores int       `gorm:"not null" json:"capacidade_jogadores" binding:"required,min=1"`
-	PriceHour           float64   `gorm:"type:decimal(10,2);not null" json:"price_hour" binding:"required,gt=0"` // valor em centavos
-	Description         string    `gorm:"type:text" json:"description"`
-	Active              bool      `gorm:"default:true" json:"active"`
-	CreatedAt           time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt           time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID              uint32         `gorm:"primaryKey;autoIncrement" json:"id"`
+	EstablishmentID uint32         `gorm:"not null;index" json:"establishment_id" binding:"required"`
+	Name            string         `gorm:"type:varchar(255);not null" json:"name" binding:"required,min=3,max=100"`
+	PlayerCapacity  int            `gorm:"not null" json:"player_capacity" binding:"required,min=1,max=100"`
+	PriceHour       float64        `gorm:"type:decimal(10,2);not null" json:"price_hour" binding:"required,min=0"` // valor em centavos
+	Description     string         `gorm:"type:text" json:"description"`
+	Active          bool           `gorm:"default:true" json:"active"`
+	CreatedAt       time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt       time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-"`
 
-	// Relacionamento reverso (belongs to)
-	Establishment Establishment `gorm:"foreignKey:EstablishmentID" json:"-"`
+	Establishment  Establishment    `gorm:"foreignKey:EstablishmentID" json:"-"`
+	AvaliableTimes []AvaliableTimes `gorm:"foreignKey:ArenaID" json:"avaliable_times,omitempty"`
+	Reservations   []Reservation    `gorm:"foreignKey:ArenaID" json:"reservations,omitempty"`
+	Matchs         []Match          `gorm:"foreignKey:ArenaID" json:"matchs,omitempty"`
 }
