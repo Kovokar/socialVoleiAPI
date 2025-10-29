1c120eb8b6bc046aa6a3f63da60d77295d7a896e
kovokar <pkovokar@gmail.com>
2025-10-29 01:35:09 -0300

    feat: implement jwt

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"socialVoleiAPI/internal/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const Bearer_schema = "Bearer "
+		header := c.GetHeader("Authorization")
+		if header == "" {
+			c.AbortWithStatus(401)
+		}
+
+		token := header[len(Bearer_schema):]
+
+		if !utils.NewJtw().ValidateToken(token) {
+			c.AbortWithStatus(401)
+		}
+	}
+}
